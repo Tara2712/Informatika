c0a1fc5293a7403d6586587f47657687381ebb9e
Kaja Vidmar <kaja.vidmar@student.um.si>
2025-05-30 15:05:55 +0200

    dodan cors manegment v main.py

--- ML_api/main.py
+++ ML_api/main.py
@@ -2,8 +2,18 @@ from fastapi import FastAPI, Query
 from pydantic import BaseModel
 from model_loader import initialize_model_and_embeddings
 from search_engine import search
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5000"],  # SAMO ZA DEV!! omeji na ["http://localhost:5000"] za produkcijo
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 model, df = initialize_model_and_embeddings()
 
 class SearchRequest(BaseModel):

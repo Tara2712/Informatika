95351d542c7cb3501eb84dc67d909a519ac675aa
Kaja Vidmar <kaja.vidmar@student.um.si>
2025-06-06 02:00:30 +0200

    docker za ml api

--- ML_api/model_loader.py
+++ ML_api/model_loader.py
@@ -5,7 +5,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 
 # def load_data(path='data/vzorec_podatkov.csv'):
-def load_data(path='data/df_no_nan_img.csv'):
+def load_data(path='/app/shared_data/df_no_nan_img.csv'):
     df = pd.read_csv(path)
     for col in ["NAZIV_SR", "OPIS", "DOLGI_OPIS_X"]:
         df[col] = df[col].fillna("")
